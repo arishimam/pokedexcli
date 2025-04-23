4c1df7cd70f262f56fb15eda7decc1e77799492d
Arish Imam <arish.imam@gmail.com>
2025-04-22 21:01:07 -0700

    explore command cache added

--- internal/pokeapi/pokemon_list.go
+++ internal/pokeapi/pokemon_list.go
@@ -12,6 +12,17 @@ func (c *Client) ListPokemon(name string) (LocationDetails, error) {
 
 	url := baseURL + fmt.Sprintf("/location-area/%v", name)
 
+	if entry, exists := c.cache.Get(url); exists {
+		fmt.Println("USED CACHE")
+		locationDetails := LocationDetails{}
+		err := json.Unmarshal(entry, &locationDetails)
+		if err != nil {
+			log.Fatal(err)
+			return LocationDetails{}, err
+		}
+		return locationDetails, nil
+	}
+
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatal(err)
@@ -37,6 +48,9 @@ func (c *Client) ListPokemon(name string) (LocationDetails, error) {
 		log.Fatal(err)
 		return LocationDetails{}, nil
 	}
+
+	c.cache.Add(url, body)
+	fmt.Println("DIDNT USED CACHE")
 
 	return locationDetails, nil
 }
